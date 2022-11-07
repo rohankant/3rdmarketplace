021f46a3549efd3eda253d0cebfe7aa04a5a2423
Rohan Kant <85503948+rohankant@users.noreply.github.com>
2022-11-07 16:05:24 +0530

    Update contractAddresses.ts

--- config/contractAddresses.ts
+++ config/contractAddresses.ts
@@ -11,7 +11,7 @@ interface IAddresses {
  */
 const ADDRESSES: IAddresses = {
   [ChainId.Goerli]: {
-    Marketplace: "0x14f3d32B90Ede9922Fd7EE4FDfD3AabbEA423aD0",
+    Marketplace: "0x33F41eB8e9D77290daD89bDD85BaE0249bc8Bdb8",
   },
 };
 
